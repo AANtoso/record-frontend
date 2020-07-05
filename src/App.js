9a24c848839eb6c4e9dd4ec3c934c33c6977813c
AANtoso <adjoaan@gmail.com>
2020-07-05 12:39:53 -0400

    Added imports, routes, and render form.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {Route, NavLink, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import DiagnosesContainer from './containers/DiagnosesContainer';
+import Diagnosis from './components/Diagnosis';
+import {fetchDiagnoses} from './actions/fetchDiagnoses';
 // import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component() {
+class App extends Component {
+
+  componentDidMount() {
+    this.props.fetchDiagnoses();
+  }
+
   render() {
     return (
-    <div className="App">
-      App
-    </div>
+      <>
+      <NavLink to='/'>Home</NavLink>
+      <br/>
+      <NavLink to='/diagnoses'>All Diagnoses</NavLink>
+      <br/><br/>
+
+      <Switch>
+        <Route path='/diagnoses/:id' render={(routerProps) => <Diagnosis {...routerProps} diagnoses={this.props.diagnoses} />} />
+        <Route path='/diagnoses' render={(routerProps) => <DiagnosesContainer {...routerProps} diagnoses={this.props.diagnoses} />} />
+        <Route path='/' component={Home} />
+      </Switch>
+      </>
   );
   }
 }
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    diagnoses: state.diagnoses
+  }
+}
+
+export default connect(mapStateToProps, {fetchDiagnoses})(App)
