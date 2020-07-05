b0e4c621c8c3807f0ee908a9f6949023dc99b485
AANtoso <adjoaan@gmail.com>
2020-07-05 13:14:19 -0400

    Created medications component.

---
+++ src/components/Medications.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+import Medication from '../components/Medication'
+
+const Medications = props => {
+    return (
+        <div>
+            {props.medications && props.medications.map(medication =>
+                <Card key={medication.id}>
+                    <Card.Header>
+                        <Medication medication={medication.name} id={medication.id} diagnosisID={medication.diagnosis_id} />
+                    </Card.Header>
+                </Card>
+            )}
+        </div>
+    );
+}
+
+export default Medications;
