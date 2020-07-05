411976c919df2ad466486368f9fb74c8957761be
AANtoso <adjoaan@gmail.com>
2020-07-05 12:49:19 -0400

    Added imports and render.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+// import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import diagnosisReducer from './reducers/diagnosisReducer';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+// import * as serviceWorker from './serviceWorker';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+let store = createStore(diagnosisReducer, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <Router>
+      <Container>
+        <App />
+      </Container>
+    </Router>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
