d2d04d35fdaf5f4d73a3314b2e1b075671798e3d
AANtoso <adjoaan@gmail.com>
2020-07-05 11:25:57 -0400

    Created delete action.

---
+++ src/actions/deleteMedication.js
@@ -0,0 +1,9 @@
+export const addMedication = (medicationID, diagnosisID) => {
+    return dispatch => {
+        fetch(`http://localhost:3001/api/v1/diagnoses/${diagnosisID}/medications/${medicationID}`, {
+            method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(diagnosis => dispatch({ type:'DELETE_MEDICATION', payload: diagnosis }))
+    }
+}
