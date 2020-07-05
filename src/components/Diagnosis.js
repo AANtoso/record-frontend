c2b1497c8c33fc429df0a696f8f63522f1cdc8cf
AANtoso <adjoaan@gmail.com>
2020-07-05 13:41:09 -0400

    Fixed spelling errors.

--- src/components/Diagnosis.js
+++ src/components/Diagnosis.js
@@ -2,7 +2,7 @@ import React from 'react';
 import MedicationsContainer from '../containers/MedicationsContainer';
 
 const Diagnosis = props => {
-    const dagnosis = props.diagnoses[parseInt(props.match.params.id) - 1]
+    const diagnosis = props.diagnoses[parseInt(props.match.params.id) - 1]
     return (
      <>
         <h3>{diagnosis ? diagnosis.name :null}</h3>
