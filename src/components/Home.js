42ee69cc4e3a8dec20b22621dde488b512c608eb
AANtoso <adjoaan@gmail.com>
2020-07-05 12:53:18 -0400

    Created home compoent.

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h2 style={{textAlign: 'center'}}>HealthRecord</h2>
+            <p style={{textAlign: 'center'}}> Pick a Diagnosis <br/>
+            Click on each diagnosis to enter a medication and whether it has been called into the pharmacy or not.</p>
+        </div>
+    );
+}
+
+export default Home;
