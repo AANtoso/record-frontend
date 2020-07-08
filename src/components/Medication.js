13433e2968ac073508436884b2f2b946a8546720
AANtoso <adjoaan@gmail.com>
2020-07-08 17:41:45 -0400

    Moved and changed delete.

--- src/components/Medication.js
+++ src/components/Medication.js
@@ -1,30 +1,29 @@
 import React from 'react';
 import Accordion from 'react-bootstrap/Accordion';
 import Button from 'react-bootstrap/Button';
-import {deleteMedication} from '../actions/deleteMedication'
-import { connect } from 'react-redux'
+// import {deleteMedication} from '../actions/deleteMedication'
+// import { connect } from 'react-redux'
 
 const Medication = props => {
 // debugger
 
-    const handleDelete = (event) => {
-        // debugger
-        const medId = event.target.parentElement.parentElement.dataset.id
-        const dId = event.target.parentElement.parentElement.dataset.diagnosisId
-        event.target.parentElement.parentElement.remove()
-        props.deleteMedication(medId, dId)
-    }
+    // const handleDelete = (event) => {
+    //     const medId = event.target.parentElement.parentElement.dataset.id
+    //     const dId = event.target.parentElement.parentElement.dataset.diagnosisId
+    //     event.target.parentElement.parentElement.remove()
+    //     props.deleteMedication(medId, dId)
+    // }
 
     return (
-        <div data-id={props.id} data-diagnosis-id={props.diagnosisID}>
+        <div>
             <Accordion defaultActiveKey>
                 <Accordion.Toggle as={Button} variant='info' >
                     {props.medication}
                 </Accordion.Toggle>
-                <Button size='sm' variant='outline-danger' onClick={handleDelete}>Delete</Button>
+                {/* <Button size='sm' variant='outline-danger' onClick={handleDelete}>Delete</Button> */}
             </Accordion>
         </div>
     )
 }
 
-export default connect(null, {deleteMedication})(Medication);
+export default Medication;
--- src/components/Medications.js
+++ src/components/Medications.js
@@ -2,16 +2,31 @@ import React from 'react';
 import Card from 'react-bootstrap/Card';
 import Medication from '../components/Medication'
 import { connect } from 'react-redux';
+import Button from 'react-bootstrap/Button';
+import {deleteMedication} from '../actions/deleteMedication'
 
 const Medications = props => {
+
+    const handleDelete = (event) => {
+        // debugger
+        const medId = event.target.parentElement.parentElement.dataset.id
+        const dId = event.target.parentElement.parentElement.dataset.diagnosisId
+        event.target.parentElement.parentElement.remove()
+        props.deleteMedication(medId, dId)
+    }
+
     const renderMedications = props.medications.map(medication => {
-        //debugger
+        // debugger
         return (
-             <Card key={medication.id}>
+            <div data-id={medication.id} data-diagnosis-id={medication.diagnosis_id} key={medication.id}>             
+                <Card>
                 <Card.Header>
                     <Medication medication={medication.name} id={medication.id} diagnosisID={medication.diagnosis_id} />
                 </Card.Header>
+                <Button size='sm' variant='outline-danger' onClick={handleDelete}>Delete</Button>
             </Card>
+            </div>
+
                 )
     })
     return (
@@ -25,4 +40,4 @@ const mapStateToProps = state => ({
     medications: state.medications
 })
 
-export default connect(mapStateToProps, {})(Medications);
+export default connect(mapStateToProps, {deleteMedication})(Medications);
